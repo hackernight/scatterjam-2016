cbe59c58cf02c0063b85e1afdb23b7870ccd6360
rasbury <rasbury7@gmail.com>
2016-11-05 11:39:07 -0500

    added colin mocherie

--- www/js/states/init.js
+++ www/js/states/init.js
@@ -1,9 +1,11 @@
 var state_init = function(game) {
     return {
         preload: function() {
+              game.load.image('colin1', 'assets/colin1.jpg');
         },
 
         update: function() {
+          game.add.sprite(0, 0, 'colin1');
         }
     };
 };
