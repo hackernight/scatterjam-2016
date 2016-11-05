3dbad2423b55d21518ec9af47583ca4742747297
rasbury <rasbury7@gmail.com>
2016-11-05 11:49:07 -0500

    there can be only one colin

--- www/js/states/init.js
+++ www/js/states/init.js
@@ -2,10 +2,15 @@ var state_init = function(game) {
     return {
         preload: function() {
               game.load.image('colin1', 'assets/colin1.jpg');
+              game.load.image('judgeResponse', 'assets/judgeResponse.jpg');
+        },
+
+        create: function() {
+          game.add.sprite(0, 0, 'colin1');
         },
 
         update: function() {
-          game.add.sprite(0, 0, 'colin1');
+
         }
     };
 };
