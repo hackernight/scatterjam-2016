c28574f28509dfaddc5dcd6bebcaf885cec9cc1a
Evan Cobb <cobb.evan@gmail.com>
2016-11-06 09:57:00 -0600

    Feet now show and move with the legs!

--- www/js/states/dancing.js
+++ www/js/states/dancing.js
@@ -104,9 +104,9 @@ function pose() {
     gameState.dancer.rightArm.angle = currentDance.pose.rightArm;
     gameState.dancer.rightHand.angle = 180 + currentDance.pose.rightArm;
     gameState.dancer.leftLeg.angle = currentDance.pose.leftLeg;
-    // gameState.dancer.leftFoot.angle = currentDance.pose.leftLeg;
+    gameState.dancer.leftFoot.angle = currentDance.pose.leftLeg;
     gameState.dancer.rightLeg.angle = currentDance.pose.rightLeg;
-    // gameState.dancer.rightFoot.angle = currentDance.pose.rightLeg;
+    gameState.dancer.rightFoot.angle = currentDance.pose.rightLeg;
 }
 
 function constructDancer() {
@@ -134,12 +134,12 @@ function constructDancer() {
     dancer.rightLeg = makeBodyPart('dancer-rightLeg', 20, 35, gameState.currentDancer);
     dancer.rightLeg.anchor.setTo(0.5, 0);
 
-    // dancer.leftFoot = makeBodyPart('dancer-leftFoot', -25, 135 + dancer.leftLeg.height, gameState.currentDancer);
-    // dancer.leftFoot.anchor.setTo(0.5, 0.5);
-    // dancer.leftFoot.pivot.y = dancer.leftLeg.height;
-    // dancer.rightFoot = makeBodyPart('dancer-rightFoot', 25, 135, gameState.currentDancer);
-    // dancer.rightFoot.anchor.setTo(0.5, 0.5);
-    // dancer.rightFoot.pivot.y = dancer.rightLeg.height;
+    dancer.leftFoot = makeBodyPart('dancer-leftFoot', -25, 135 - dancer.leftLeg.height, gameState.currentDancer);
+    dancer.leftFoot.anchor.setTo(0.5, 0.5);
+    dancer.leftFoot.pivot.y = -dancer.leftLeg.height;
+    dancer.rightFoot = makeBodyPart('dancer-rightFoot', 25, 135 - dancer.leftLeg.height, gameState.currentDancer);
+    dancer.rightFoot.anchor.setTo(0.5, 0.5);
+    dancer.rightFoot.pivot.y = -dancer.rightLeg.height;
 
     return dancer;
 }
