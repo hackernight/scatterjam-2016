5f15d2e2a3169325a326fb1678b50af1a7ce7d95
rasbury <rasbury7@gmail.com>
2016-11-06 12:54:26 -0600

    Revert "Added Head Ass et to title screen"
    This reverts commit 49136d50e89186f569fcabd72a5edf3de284fc0b.

--- www/js/states/startMenu.js
+++ www/js/states/startMenu.js
@@ -44,7 +44,6 @@ var startMenu = function(game) {
             game.load.image('judge2', 'assets/judge/2Judge/normalSprite.png');
             game.load.image('judge3', 'assets/judge/3Judge/normalSprite.png');
             game.load.image('audience', 'assets/audience/audience.png');
-            game.load.image('headAss', 'assets/intro/headAss.png');
 
             game.load.image('dancer-head', 'assets/dancer/mikeDancer/head.png');
             game.load.image('dancer-leftArm', 'assets/dancer/mikeDancer/leftArm.png');
@@ -68,10 +67,7 @@ var startMenu = function(game) {
 
             game.load.onLoadComplete.add(function() {
                 // TODO kill this so you have an actual start menu
-                //game.state.start('dancing');
-
-                headAss = game.add.sprite(0, 0, 'headAss');
-                headAss.scale.setTo(6, 6);
+                game.state.start('dancing');
 
                 this.loadingText.destroy();
                 var startButton = this.centeredText("Start Judging", game.world.centerY);
@@ -86,9 +82,9 @@ var startMenu = function(game) {
                           //align: "center",
                           backgroundColor: "black"
                       };
+
                         backstory = game.add.text(32, 32, '', style);
                         startButton.destroy();
-                        headAss.destroy();
                         displayBackstory();
 
                     });
