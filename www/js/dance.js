1a0b9f6edf316511c5635195e9680e04c0b1ae87
rasbury <rasbury7@gmail.com>
2016-11-06 18:59:41 -0600

    Dance moves are randomly selected

--- www/js/dance.js
+++ www/js/dance.js
@@ -1,5 +1,6 @@
 var createDance = function() {
-    var danceMoves = [{
+
+    var restingState ={
         name: "",
         score: function() {
             return 0;
@@ -10,73 +11,83 @@ var createDance = function() {
             leftLeg: 0,
             rightLeg: 0,
         }
-    }, {
-        name: "Jazz Hands",
-        score: function() {
-            return 3;
-        },
-        pose: {
-            leftArm: 150,
-            rightArm: -150,
-            leftLeg: 0,
-            rightLeg: 0,
-        }
-    }, {
-        name: "Catching Air",
-        score: function() {
-            return 2;
-        },
-        pose: {
-            leftArm: 0,
-            rightArm: 0,
-            leftLeg: 90,
-            rightLeg: -90,
-        }
-    }, {
-        name: "Reeling Starfish",
-        score: function() {
-            return -2;
-        },
-        pose: {
-            leftArm: 135,
-            rightArm: -135,
-            leftLeg: 45,
-            rightLeg: -45,
-        }
-    }, {
-        name: "Moonwalk",
-        score: function() {
-            return 1;
-        },
-        pose: {
-            leftArm: 0,
-            rightArm: 0,
-            leftLeg: -30,
-            rightLeg: 30,
-        }
-    }, {
-        name: "Big Finish",
-        score: function() {
-            return 2;
-        },
-        pose: {
-            leftArm: 135,
-            rightArm: -135,
-            leftLeg: 0,
-            rightLeg: 0,
-        }
-    }, {
-        name: "",
-        score: function() {
-            return 0;
-        },
-        pose: {
-            leftArm: 0,
-            rightArm: 0,
-            leftLeg: 0,
-            rightLeg: 0,
-        }
-    }, ];
+    };
+
+    function getRandomMove(){
+
+        var randomizedNumber = (game.rnd.integerInRange(1, 5));
+
+            switch(randomizedNumber) {
+              case 1:
+              return {
+                  name: "Jazz Hands",
+                  score: function() {
+                      return 3;
+                  },
+                  pose: {
+                      leftArm: 150,
+                      rightArm: -150,
+                      leftLeg: 0,
+                      rightLeg: 0,
+                  }
+              };
+              case 2:
+              return {
+                  name: "Catching Air",
+                  score: function() {
+                      return 2;
+                  },
+                  pose: {
+                      leftArm: 0,
+                      rightArm: 0,
+                      leftLeg: 90,
+                      rightLeg: -90,
+                  }
+              };
+              case 3:
+              return {
+                  name: "Reeling Starfish",
+                  score: function() {
+                      return -2;
+                  },
+                  pose: {
+                      leftArm: 135,
+                      rightArm: -135,
+                      leftLeg: 45,
+                      rightLeg: -45,
+                  }
+              };
+              case 4:
+              return {
+                  name: "Moonwalk",
+                  score: function() {
+                      return 1;
+                  },
+                  pose: {
+                      leftArm: 0,
+                      rightArm: 0,
+                      leftLeg: -30,
+                      rightLeg: 30,
+                  }
+              };
+              case 5:
+              return {
+                  name: "Big Finish",
+                  score: function() {
+                      return 2;
+                  },
+                  pose: {
+                      leftArm: 135,
+                      rightArm: -135,
+                      leftLeg: 0,
+                      rightLeg: 0,
+                  }
+              };
+              }
+
+    }
+
+    var danceMoves = [restingState, getRandomMove(), getRandomMove(), getRandomMove(), getRandomMove(), getRandomMove(), restingState, ];
     //expansion ideas:
     //the robot
     //ballerina pirouette
