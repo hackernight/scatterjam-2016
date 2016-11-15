95f1ce1e1b6142759ba0b3e59c82038bfcaaf69e
Evan Cobb <cobb.evan@gmail.com>
2016-11-15 11:47:31 -0600

    WIP - start menu works!

--- src/states/startMenu.js
+++ src/states/startMenu.js
@@ -28,101 +28,99 @@
 
 class StartMenu extends Phaser.State {
     preload() {
-        game.stage.backgroundColor = "#aaaaaa";
+        this.game.stage.backgroundColor = "#aaaaaa";
         this.game.scale.pageAlignHorizontally = true;
         this.game.scale.pageAlignVertically = true;
         this.game.scale.refresh();
 
         this.centeredText("Russian Judge Redemption", 20);
-        this.loadingText = this.centeredText("Loading", game.world.centerY);
-        game.load.image('floor', 'assets/floorTile/draftFloor.png');
-        game.load.image('judgeResponseGood', 'assets/judgeResponseGood.jpg');
-        game.load.image('judgeResponseBad', 'assets/judgeResponseBad.jpg');
-        game.load.image('feedbackResultGood', 'assets/feedback/happySprite.png');
-        game.load.image('feedbackResultBad', 'assets/feedback/sadSprite.png');
-        game.load.image('meterArrow', 'assets/meter-arrow.png');
-        game.load.image('voteScoreBar', 'assets/vote-score-bar.png');
-        game.load.image('russianJudge', 'assets/judge/russianJudge/normalSprite.png');
-        game.load.image('judge1', 'assets/judge/1Judge/normalSprite.png');
-        game.load.image('judge2', 'assets/judge/2Judge/normalSprite.png');
-        game.load.image('judge3', 'assets/judge/3Judge/normalSprite.png');
-        game.load.image('audience', 'assets/audience/audience.png');
-        game.load.image('headAss', 'assets/intro/headAss.png');
-        game.load.image('judge1Score', 'assets/judge/1Judge/scoreSprite.png');
-        game.load.image('judge2Score', 'assets/judge/2Judge/scoreSprite.png');
-        game.load.image('judge3Score', 'assets/judge/3Judge/scoreSprite.png');
-        game.load.image('russianJudgeScore', 'assets/judge/russianJudge/scoreSprite.png');
+        this.loadingText = this.centeredText("Loading", this.game.world.centerY);
+        this.game.load.image('floor', 'assets/floorTile/draftFloor.png');
+        this.game.load.image('judgeResponseGood', 'assets/judgeResponseGood.jpg');
+        this.game.load.image('judgeResponseBad', 'assets/judgeResponseBad.jpg');
+        this.game.load.image('feedbackResultGood', 'assets/feedback/happySprite.png');
+        this.game.load.image('feedbackResultBad', 'assets/feedback/sadSprite.png');
+        this.game.load.image('meterArrow', 'assets/meter-arrow.png');
+        this.game.load.image('voteScoreBar', 'assets/vote-score-bar.png');
+        this.game.load.image('russianJudge', 'assets/judge/russianJudge/normalSprite.png');
+        this.game.load.image('judge1', 'assets/judge/1Judge/normalSprite.png');
+        this.game.load.image('judge2', 'assets/judge/2Judge/normalSprite.png');
+        this.game.load.image('judge3', 'assets/judge/3Judge/normalSprite.png');
+        this.game.load.image('audience', 'assets/audience/audience.png');
+        this.game.load.image('headAss', 'assets/intro/headAss.png');
+        this.game.load.image('judge1Score', 'assets/judge/1Judge/scoreSprite.png');
+        this.game.load.image('judge2Score', 'assets/judge/2Judge/scoreSprite.png');
+        this.game.load.image('judge3Score', 'assets/judge/3Judge/scoreSprite.png');
+        this.game.load.image('russianJudgeScore', 'assets/judge/russianJudge/scoreSprite.png');
 
-        game.load.image('dancer-head', 'assets/dancer/mikeDancer/head.png');
-        game.load.image('dancer-leftArm', 'assets/dancer/mikeDancer/leftArm.png');
-        game.load.image('dancer-rightArm', 'assets/dancer/mikeDancer/rightArm.png');
-        game.load.image('dancer-leftHand', 'assets/dancer/mikeDancer/leftHand.png');
-        game.load.image('dancer-rightHand', 'assets/dancer/mikeDancer/rightHand.png');
-        game.load.image('dancer-leftFoot', 'assets/dancer/mikeDancer/leftFoot.png');
-        game.load.image('dancer-rightFoot', 'assets/dancer/mikeDancer/rightFoot.png');
-        game.load.image('dancer-leftLeg', 'assets/dancer/mikeDancer/leftLeg.png');
-        game.load.image('dancer-rightLeg', 'assets/dancer/mikeDancer/rightLeg.png');
-        game.load.image('dancer-torso', 'assets/dancer/mikeDancer/torso.png');
+        this.game.load.image('dancer-head', 'assets/dancer/mikeDancer/head.png');
+        this.game.load.image('dancer-leftArm', 'assets/dancer/mikeDancer/leftArm.png');
+        this.game.load.image('dancer-rightArm', 'assets/dancer/mikeDancer/rightArm.png');
+        this.game.load.image('dancer-leftHand', 'assets/dancer/mikeDancer/leftHand.png');
+        this.game.load.image('dancer-rightHand', 'assets/dancer/mikeDancer/rightHand.png');
+        this.game.load.image('dancer-leftFoot', 'assets/dancer/mikeDancer/leftFoot.png');
+        this.game.load.image('dancer-rightFoot', 'assets/dancer/mikeDancer/rightFoot.png');
+        this.game.load.image('dancer-leftLeg', 'assets/dancer/mikeDancer/leftLeg.png');
+        this.game.load.image('dancer-rightLeg', 'assets/dancer/mikeDancer/rightLeg.png');
+        this.game.load.image('dancer-torso', 'assets/dancer/mikeDancer/torso.png');
 
-        game.load.image('happyEmote', 'assets/judge/emotes/happyEmote.png');
-        game.load.image('neutralEmote', 'assets/judge/emotes/neutralEmote.png');
-        game.load.image('madEmote', 'assets/judge/emotes/madEmote.png');
+        this.game.load.image('happyEmote', 'assets/judge/emotes/happyEmote.png');
+        this.game.load.image('neutralEmote', 'assets/judge/emotes/neutralEmote.png');
+        this.game.load.image('madEmote', 'assets/judge/emotes/madEmote.png');
 
-        game.load.image('storyBackground', 'assets/intro/storyBackground.png');
-        game.load.image('stamp', 'assets/intro/hammerSickle.png');
+        this.game.load.image('storyBackground', 'assets/intro/storyBackground.png');
+        this.game.load.image('stamp', 'assets/intro/hammerSickle.png');
 
-        game.load.audio('polka', 'assets/audio/BavariaShort.mp3');
-        game.load.audio('polka2', 'assets/audio/BlueSkaShort.mp3');
-        game.load.audio('polka3', 'assets/audio/DarxieLandShort.mp3');
-        game.load.audio('polka4', 'assets/audio/SunflowerDancePartyShort.mp3');
-        game.load.audio('dark', 'assets/audio/GloomShort.mp3');
-        game.load.audio('boo', 'assets/audio/BooShort.mp3');
-        game.load.audio('cheer', 'assets/audio/CheerShort.mp3');
+        this.game.load.audio('polka', 'assets/audio/BavariaShort.mp3');
+        this.game.load.audio('polka2', 'assets/audio/BlueSkaShort.mp3');
+        this.game.load.audio('polka3', 'assets/audio/DarxieLandShort.mp3');
+        this.game.load.audio('polka4', 'assets/audio/SunflowerDancePartyShort.mp3');
+        this.game.load.audio('dark', 'assets/audio/GloomShort.mp3');
+        this.game.load.audio('boo', 'assets/audio/BooShort.mp3');
+        this.game.load.audio('cheer', 'assets/audio/CheerShort.mp3');
 
-        game.load.image('particle-star', 'assets/particle-star.png');
+        this.game.load.image('particle-star', 'assets/particle-star.png');
 
-        game.load.onLoadComplete.add(function() {
+        this.game.load.onLoadComplete.add(function() {
             // TODO kill this so you have an actual start menu
-            // game.state.start('dancing');
+            // this.game.state.start('dancing');
 
-            headAss = game.add.sprite(0, 0, 'headAss');
+            let headAss = this.game.add.sprite(0, 0, 'headAss');
             headAss.scale.setTo(6, 6);
 
             this.loadingText.destroy();
-            var startButton = this.centeredText("Start Judging", game.world.centerY);
-            create();
+            var startButton = this.centeredText("Start Judging", this.game.world.centerY);
+            this.createTitleFluff();
 
             startButton.inputEnabled = true;
             startButton.events.onInputDown.add(
-                function() {
-                    game.state.start('introText');
+                () => {
+                    this.game.state.start('introText');
                 });
         }, this);
     }
     centeredText(ctext, y) {
-        ctext = game.add.text(game.world.centerX, y, ctext);
+        ctext = this.game.add.text(this.game.world.centerX, y, ctext);
         ctext.anchor.setTo(0.5, 0.5);
         return ctext;
     }
 
-};
-var rJudges;
+    createTitleFluff() {
+        let rJudges = this.game.add.group();
+        rJudges.enableBody = true;
+        rJudges.physicsBodyType = Phaser.Physics.ARCADE;
 
-function create() {
+        for (var i = 0; i < 10; i++) {
+            var headAss = rJudges.create(200 + i * 48, -50, 'stamp');
 
-    rJudges = game.add.group();
-    rJudges.enableBody = true;
-    rJudges.physicsBodyType = Phaser.Physics.ARCADE;
-
-    for (var i = 0; i < 10; i++) {
-        var headAss = rJudges.create(200 + i * 48, -50, 'stamp');
-
-        //This allows your sprite to collide with the world bounds like they were rigid objects
-        headAss.body.collideWorldBounds = true;
-        headAss.body.gravity.x = game.rnd.integerInRange(-50, 50);
-        headAss.body.gravity.y = 100 + Math.random() * 100;
-        headAss.body.bounce.setTo(0.9, 0.9);
+            //This allows your sprite to collide with the world bounds like they were rigid objects
+            headAss.body.collideWorldBounds = true;
+            headAss.body.gravity.x = this.game.rnd.integerInRange(-50, 50);
+            headAss.body.gravity.y = 100 + Math.random() * 100;
+            headAss.body.bounce.setTo(0.9, 0.9);
+        }
     }
-}
+
+};
 
 export default StartMenu;
